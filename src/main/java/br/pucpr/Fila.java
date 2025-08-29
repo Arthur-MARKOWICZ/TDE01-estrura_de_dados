55517351816bfc01d5b550d6774140cb33ca9695
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-08-29 10:44:51 -0300

    arrumando a fila para ter uma matrix

--- src/main/java/br/pucpr/Fila.java
+++ src/main/java/br/pucpr/Fila.java
@@ -1,29 +1,40 @@
 package br.pucpr;
 
 public class Fila {
-    private int[] dados;
-    private  int inicio;
-    private int fim;
-    private  int   tamamanho;
-    public Fila(int capacidade){
-        dados = new int[capacidade];
-        inicio = 0;
-        fim = 0;
-        tamamanho = 0;
-    }
-    public void  adicionar(int valor){
-        if(fim == dados.length){
-            throw  new RuntimeException("fila completa");
+    private int[][] dados;
+    private int [] inicio;
+    private int []fim;
+    private int  [] tamanho;
+    private int capacidade;
+    public Fila(int capacidade,int Nfilas){
+        this.capacidade = capacidade;
+        dados = new int[Nfilas][capacidade];
+        inicio = new int[Nfilas];
+        fim = new int[Nfilas];
+        tamanho = new int[Nfilas];
+        for (int i = 0; i < Nfilas; i++) {
+            inicio[i] = 0;
+            fim[i] = -1;
+            tamanho[i] = 0;
         }
-        dados[fim++] = valor;
-        tamamanho++;
     }
-    public  int remover(){
-        if(tamamanho==0){
-            throw new RuntimeException("fila vazia");
+    public void enfileirar(int filaId, int valor) {
+        if (tamanho[filaId] == capacidade) {
+            System.out.println("Fila " + filaId + " cheia!");
+            return;
         }
-        int valor = dados[inicio++];
-        tamamanho--;
+        fim[filaId] = (fim[filaId] + 1) % capacidade;
+        dados[filaId][fim[filaId]] = valor;
+        tamanho[filaId]++;
+    }
+
+    public int desenfileirar(int filaId) {
+        if (tamanho[filaId] == 0) {
+            throw new RuntimeException("Fila " + filaId + " vazia!");
+        }
+        int valor = dados[filaId][inicio[filaId]];
+        inicio[filaId] = (inicio[filaId] + 1) % capacidade;
+        tamanho[filaId]--;
         return valor;
     }
 
--- src/main/java/br/pucpr/PreencherImagemFila.java
+++ src/main/java/br/pucpr/PreencherImagemFila.java
@@ -1,5 +1,22 @@
 package br.pucpr;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.Buffer;
+
 public class PreencherImagemFila {
+    public PreencherImagemFila() throws IOException {
+    }
+    BufferedImage img = ImageIO.read(new File("golem.png"));
+    Fila fila = new Fila(img.getWidth(),img.getHeight());
+    public void PegarPontoInicial(int valor1, int valor2){
+        int cor = img.getRGB(valor1,valor2);
+    }
+
+
+
 
 }
