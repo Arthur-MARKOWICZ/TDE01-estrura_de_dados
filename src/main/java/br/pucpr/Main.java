fb0af20475bd601f1b683903945f932c9117cf87
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-09-03 16:54:53 -0300

    fazendo testes

--- src/main/java/br/pucpr/Main.java
+++ src/main/java/br/pucpr/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         PreencherImagemFila test = new PreencherImagemFila();
-        test.PegarPontoInicial(15,15);
+        test.PegarPontoInicial(10,10);
         test.ColorirImagem();
     }
 }
